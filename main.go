7b4f4ac0e6c0b2c4055cb849cfd7430acf6d7979
David Gaspar <david.correa@intelbras.com.br>
2022-06-07 04:20:09 +0100

    Implementing GPIO

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/stianeikeland/go-rpio"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	err := rpio.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	defer rpio.Close()
+
+	pin := rpio.Pin(2)
+	pin.Input()
+
+	fmt.Println("[ PIN 2 ] Read:", pin.Read())
 }
