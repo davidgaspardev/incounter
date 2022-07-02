5295e56dd256d0e883e30bd6df7a1fa2e8d262a4
David Gaspar <davidgaspar.dev@gmail.com>
2022-07-02 20:27:56 +0100

    Test in Python to mult reles

--- tests/reles.py
+++ tests/reles.py
@@ -1,1 +1,42 @@
-print("Hello World!");
+import sys
+import RPi.GPIO as gpio
+import threading
+
+def listen_signal_by_pin(pin):
+    gpio.setup(pin, gpio.IN)
+
+    last_signal = 0
+    while True:
+        signal = gpio.input(pin)
+        if signal != last_signal:
+            last_signal = signal
+            print("[ PIN", pin, "] - Signal received:", signal)
+
+def setup(pins):
+    print("pins getted:", pins)
+
+    gpio.setmode(gpio.BCM)
+
+    #for pin in pins:
+    #    listen_signal_by_pin(pin)
+    listeners = [threading.Thread(target=listen_signal_by_pin, args=(pin,)) for pin in pins]
+
+    for listener in listeners:
+        listener.start()
+    
+    for listener in listeners:
+        listener.join()
+
+def boot():
+    argc = len(sys.argv)
+    pins = []
+    if argc > 1:
+        for i in range(1, argc):
+            if sys.argv[i].isnumeric():
+                pins.append(int(sys.argv[i]))
+    if len(pins) == 0:
+        return
+    
+    setup(pins)
+
+boot()
