f7ecb8a5fd4a500721e287bea8b6e9a98d517814
David Gaspar <davidgaspar.dev@gmail.com>
2022-07-04 23:53:45 +0100

    Update code for test (pin: 2, 17 and 27)

--- main.go
+++ main.go
@@ -3,16 +3,43 @@ package main
 import (
 	"fmt"
 	"incount/src/signal"
+
+	"github.com/davidgaspardev/golog"
 )
 
 func main() {
-	receiver := signal.GetReceiverAtPin(2)
+	unblock := make(chan bool)
 
-	var count uint16
+	gpio2 := uint8(2)
+	gpio17 := uint8(17)
+	gpio27 := uint8(27)
 
-	receiver.OnSignalChange(func(signal uint8) {
-		fmt.Println(count, "- Signal:", signal)
+	receiver2 := signal.GetReceiverAtPin(gpio2)
+	receiver17 := signal.GetReceiverAtPin(gpio17)
+	receiver27 := signal.GetReceiverAtPin(gpio27)
 
-		count++
+	var count2 uint16
+	receiver2.OnSignalChange(func(signal uint8) {
+		count2++
+		golog.Info(fmt.Sprintf("PIN %02d", gpio2), fmt.Sprintf("%2dº signal: %d", count2, signal))
 	})
+
+	var count17 uint16
+	receiver17.OnSignalChange(func(signal uint8) {
+		count17++
+		golog.Info(fmt.Sprintf("PIN %02d", gpio17), fmt.Sprintf("%2dº signal: %d", count17, signal))
+	})
+
+	var count27 uint16
+	receiver27.OnSignalChange(func(signal uint8) {
+		count27++
+		golog.Info(fmt.Sprintf("PIN %02d", gpio27), fmt.Sprintf("%2dº signal: %d", count27, signal))
+	})
+
+	for {
+		if <-unblock {
+			// Finish program
+			break
+		}
+	}
 }
